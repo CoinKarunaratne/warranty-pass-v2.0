0d25402fe79fcea23a3634e211447e59dce3f24f
A Karunaratne <coinkarunaratne@gmail.com>
2023-05-07 00:14:04 +1200

    commit

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,81 +1,81 @@
-// "use client";
+"use client";
 
-// import axios from "axios";
-// import { useQuery } from "@tanstack/react-query";
-// import { PostType } from "../../lib/types/Post";
-// import { CardDemo } from "../components/Post";
-// import Sidebar from "../components/Sidebar";
-// import Error from "../components/Error";
-// import Loading from "../components/Loading";
-// import Search from "../components/Search";
-// import { useState } from "react";
-// import { Button } from "../components/ui/button";
+import axios from "axios";
+import { useQuery } from "@tanstack/react-query";
+import { PostType } from "../../lib/types/Post";
+import { CardDemo } from "../components/Post";
+import Sidebar from "../components/Sidebar";
+import Error from "../components/Error";
+import Loading from "../components/Loading";
+import Search from "../components/Search";
+import { useState } from "react";
+import { Button } from "../components/ui/button";
 
-// const allPosts = async () => {
-//   const response = await axios.get("api/posts/getPost");
-//   return response.data;
-// };
+const allPosts = async () => {
+  const response = await axios.get("api/posts/getPost");
+  return response.data;
+};
 
-// const Dashboard = ({}) => {
-//   const [search, setSearch] = useState("");
-//   const { data, error, isLoading } = useQuery<PostType[]>({
-//     queryFn: allPosts,
-//     queryKey: ["posts"],
-//   });
-//   if (error) return <Error error={error} />;
-//   if (isLoading) return <Loading />;
-//   const handleSearch = async (value: string) => {
-//     await setSearch(value);
+const Dashboard = ({}) => {
+  const [search, setSearch] = useState("");
+  const { data, error, isLoading } = useQuery<PostType[]>({
+    queryFn: allPosts,
+    queryKey: ["posts"],
+  });
+  if (error) return <Error error={error} />;
+  if (isLoading) return <Loading />;
+  const handleSearch = async (value: string) => {
+    await setSearch(value);
 
-//     console.log(search);
-//   };
-//   const clearSearch = () => {
-//     setSearch("");
-//   };
-//   return (
-//     <div className="flex flex-row gap-1">
-//       <Sidebar />
-//       <div className="flex flex-col w-full">
-//         <Search handleSearch={handleSearch} />
-//         {search !== "" && (
-//           <Button onClick={clearSearch} variant="link" className="mt-1">
-//             Clear search
-//           </Button>
-//         )}
-//         <div className="h-auto min-h-screen p-10 w-full grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4 justify-items-center">
-//           {search !== ""
-//             ? data
-//                 ?.filter((posts) => posts.product === search)
-//                 .map((posts) => (
-//                   <CardDemo
-//                     key={posts.id}
-//                     id={posts.id}
-//                     createdAt={posts.createdAt}
-//                     date={posts.date}
-//                     expiryDate={posts.expiryDate}
-//                     period={posts.period}
-//                     product={posts.product}
-//                     store={posts.store}
-//                     type={posts.type}
-//                   />
-//                 ))
-//             : data?.map((posts) => (
-//                 <CardDemo
-//                   key={posts.id}
-//                   id={posts.id}
-//                   createdAt={posts.createdAt}
-//                   date={posts.date}
-//                   expiryDate={posts.expiryDate}
-//                   period={posts.period}
-//                   product={posts.product}
-//                   store={posts.store}
-//                   type={posts.type}
-//                 />
-//               ))}
-//         </div>
-//       </div>
-//     </div>
-//   );
-// };
+    console.log(search);
+  };
+  const clearSearch = () => {
+    setSearch("");
+  };
+  return (
+    <div className="flex flex-row gap-1">
+      {/* <Sidebar />
+      <div className="flex flex-col w-full">
+        <Search handleSearch={handleSearch} />
+        {search !== "" && (
+          <Button onClick={clearSearch} variant="link" className="mt-1">
+            Clear search
+          </Button>
+        )}
+        <div className="h-auto min-h-screen p-10 w-full grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4 justify-items-center">
+          {search !== ""
+            ? data
+                ?.filter((posts) => posts.product === search)
+                .map((posts) => (
+                  <CardDemo
+                    key={posts.id}
+                    id={posts.id}
+                    createdAt={posts.createdAt}
+                    date={posts.date}
+                    expiryDate={posts.expiryDate}
+                    period={posts.period}
+                    product={posts.product}
+                    store={posts.store}
+                    type={posts.type}
+                  />
+                ))
+            : data?.map((posts) => (
+                <CardDemo
+                  key={posts.id}
+                  id={posts.id}
+                  createdAt={posts.createdAt}
+                  date={posts.date}
+                  expiryDate={posts.expiryDate}
+                  period={posts.period}
+                  product={posts.product}
+                  store={posts.store}
+                  type={posts.type}
+                />
+              ))}
+        </div>
+      </div> */}
+    </div>
+  );
+};
 
-// export default Dashboard;
+export default Dashboard;
--- app/post/[id]/page.tsx
+++ app/post/[id]/page.tsx
@@ -1,59 +1,59 @@
-// "use client";
+"use client";
 
-// import { PostType } from "../../../lib/types/Post";
-// import { useQuery } from "@tanstack/react-query";
-// import axios from "axios";
-// import Image from "next/image";
-// import { DetailedCard } from "@/app/components/DetailedCard";
+import { PostType } from "../../../lib/types/Post";
+import { useQuery } from "@tanstack/react-query";
+import axios from "axios";
+import Image from "next/image";
+import { DetailedCard } from "@/app/components/DetailedCard";
 
-// type URL = {
-//   params: {
-//     id: string;
-//   };
-// };
+type URL = {
+  params: {
+    id: string;
+  };
+};
 
-// const fetchDetails = async (id: string) => {
-//   const response = await axios.get(`/api/posts/${id}`);
-//   return response.data;
-// };
+const fetchDetails = async (id: string) => {
+  const response = await axios.get(`/api/posts/${id}`);
+  return response.data;
+};
 
-// const PostDetails = (url: URL) => {
-//   const { data, isLoading } = useQuery<PostType>({
-//     queryFn: () => fetchDetails(url.params.id),
-//     queryKey: ["detail-post"],
-//   });
-//   if (isLoading) return "Loading...";
-//   return (
-//     <div className="relative h-screen flex items-center justify-center overflow-x-hidden">
-//       <div className="container pt-32 max-w-7xl w-full mx-auto h-full">
-//         <div className="h-full gap-6 flex flex-col justify-start lg:justify-center items-center lg:items-start">
-//           <div className="relative w-full max-w-xl lg:max-w-3xl lg:left-1/2 aspect-square lg:absolute">
-//             <Image
-//               priority
-//               quality={100}
-//               style={{ objectFit: "contain" }}
-//               className="scale-125 img-shadow hidden lg:inline-block"
-//               fill
-//               src={`/ps5.png`}
-//               alt="controller"
-//             />
-//           </div>
-//           <DetailedCard
-//             key={data?.id}
-//             id={data?.id}
-//             createdAt={data?.createdAt}
-//             date={data?.date}
-//             expiryDate={data?.expiryDate}
-//             period={data?.period}
-//             product={data?.product}
-//             store={data?.store}
-//             type={data?.type}
-//             picture={data?.picture}
-//           />
-//         </div>
-//       </div>
-//     </div>
-//   );
-// };
+const PostDetails = (url: URL) => {
+  const { data, isLoading } = useQuery<PostType>({
+    queryFn: () => fetchDetails(url.params.id),
+    queryKey: ["detail-post"],
+  });
+  if (isLoading) return "Loading...";
+  return (
+    <div className="relative h-screen flex items-center justify-center overflow-x-hidden">
+      {/* <div className="container pt-32 max-w-7xl w-full mx-auto h-full">
+        <div className="h-full gap-6 flex flex-col justify-start lg:justify-center items-center lg:items-start">
+          <div className="relative w-full max-w-xl lg:max-w-3xl lg:left-1/2 aspect-square lg:absolute">
+            <Image
+              priority
+              quality={100}
+              style={{ objectFit: "contain" }}
+              className="scale-125 img-shadow hidden lg:inline-block"
+              fill
+              src={`/ps5.png`}
+              alt="controller"
+            />
+          </div>
+          <DetailedCard
+            key={data?.id}
+            id={data?.id}
+            createdAt={data?.createdAt}
+            date={data?.date}
+            expiryDate={data?.expiryDate}
+            period={data?.period}
+            product={data?.product}
+            store={data?.store}
+            type={data?.type}
+            picture={data?.picture}
+          />
+        </div>
+      </div> */}
+    </div>
+  );
+};
 
-// export default PostDetails;
+export default PostDetails;
