d54e5b584fdd55c6d476c18be5698614b48dac1f
A Karunaratne <coinkarunaratne@gmail.com>
2023-05-07 20:40:26 +1200

    commit

--- app/components/Loading.tsx
+++ app/components/Loading.tsx
@@ -9,9 +9,9 @@ const Loading = ({}) => {
       <div className="flex flex-col w-full">
         <Search />
         <div className="h-screen py-10 pl-[50px] lg:pl-[100px] w-full overflow-y-scroll grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4">
-          <Skeleton className="w-[300px] h-[400px]" />
-          <Skeleton className="w-[300px] h-[400px]" />
-          <Skeleton className="w-[300px] h-[400px]" />
+          <Skeleton className="w-auto h-[400px]" />
+          <Skeleton className="w-auto h-[400px]" />
+          <Skeleton className="w-auto h-[400px]" />
         </div>
       </div>
     </div>
